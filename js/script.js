b9b563588851b7188262cda355e68ebad46a6d9c
Iris Beci <57106955+beciweb@users.noreply.github.com>
2025-01-22 23:14:17 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,49 +1,15 @@
 $(document).ready(function () {
     // Sample data for music files (name, category, album image, mp3 path)
     const musicData = [
-        {
-            title: "Blues Jazz",
-            category: "jazz",
-            albumImage: "assets/images/bluejazz.jpg",
-            mp3File: "assets/music/blues.mp3"
-        },
-         {
-            title: "Blues The Sky is Crying",
-            category: "jazz",
-            albumImage: "assets/images/bluejazz.jpg",
-            mp3File: "assets/music/jazz2.mp3"
-        },
-           {
-            title: "Blues Dan Patlansky - Big Things Going Down",
-            category: "jazz",
-            albumImage: "assets/images/bluejazz.jpg",
-            mp3File: "assets/music/Dan Patlansky - Big Things Going Down.mp3"
-        },
-           {
-            title: "Blues Eb Davis - What You Gonna Do",
-            category: "jazz",
-            albumImage: "assets/images/bluejazz.jpg",
-            mp3File: "assets/music/Eb Davis - What You Gonna Do.mp3"
-        },
-            {
-            title: "Blues Delight - If I Had Money",
-            category: "jazz",
-            albumImage: "assets/images/bluejazz.jpg",
-            mp3File: "assets/music/Blues Delight - If I Had Money.mp3"
-        },
+
              {
             title: "Chris Bell - Elevator To Heaven",
             category: "jazz",
             albumImage: "assets/images/bluejazz.jpg",
             mp3File: "assets/music/Chris Bell - Elevator To Heaven.mp3"
         },
+       
         {
-            title: "Potpuri Shkodrane Eduart Jubani",
-            category: "shkodrane",
-            albumImage: "assets/images/potpurishkodrane.jpg",
-            mp3File: "assets/music/eduartjubani.mp3"
-        },
-           {
             title: "Ervin Bushati",
             category: "shkodrane",
             albumImage: "assets/images/eb.png",
@@ -62,62 +28,7 @@ $(document).ready(function () {
             albumImage: "assets/images/shendevere.jpg",
             mp3File: "assets/music/Sidrit Bejleri Shend e vere.mp3"
         },
-          {
-            title: "Sidrit Bejleri Ne prag te deres",
-            category: "shkodrane",
-            albumImage: "assets/images/nepragtederes.jpg",
-            mp3File: "assets/music/Sidrit Bejleri - Në prag të derës (Official Video) .mp3"
-        },
-{
-            title: "Sidrit Bejleri Pat kene nje kohe",
-            category: "shkodrane",
-            albumImage: "assets/images/patkenenjekoh.jpg",
-            mp3File: "assets/music/Sidrit Bejleri - Pat kene nji kohe.mp3"
-        },
-        {
-            title: "Sidrit Bejleri Galina",
-            category: "shkodrane",
-            albumImage: "assets/images/galina.jpg",
-            mp3File: "assets/music/Sidrit Bejleri Galina.mp3"
-        },
-
-        {
-            title: "Sidrit Bejleri A e ke ndi",
-            category: "shkodrane",
-            albumImage: "assets/images/aekendi.jpg",
-            mp3File: "assets/music/Sidrit Bejleri - A e ke ndi.mp3"
-        },
-        {
-            title: "Si ke ndje Irian Taraboshi",
-            category: "shkodrane",
-            albumImage: "assets/images/sikendje.jpg",
-            mp3File: "assets/music/SI KE NDJE_.mp3"
-        },
-        {
-            title: "Shkoj e vi nga dera jote",
-            category: "shkodrane",
-            albumImage: "assets/images/shkojevi.jpg",
-            mp3File: "assets/music/Shkoj e vi nga dera jote- Niki Mjeda.mp3"
-        },
-         {
-            title: "Frederik Ndoci - Ah ky mall",
-            category: "shkodrane",
-            albumImage: "assets/images/Fndoci.jpg",
-            mp3File: "assets/music/Frederik Ndoci - Ah ky mall (2010).mp3"
-        },
-        // Add more songs as needed
-         {
-            title: "Rame Lahaj Per mu paska ken kismet",
-            category: "shkodrane",
-            albumImage: "assets/images/ramelahaj.jpg",
-            mp3File: "assets/music/Rame Lahaj - Per mu paska kenkismet - August 2021 (1).mp3"
-        },
-        {
-            title: "Rame Lahaj - Bishtalecat Pale Pale",
-            category: "shkodrane",
-            albumImage: "assets/images/ramelahaj.jpg",
-            mp3File: "assets/music/Rame Lahaj - Bishtalecat Pale Pale - January 2023.mp3"
-        },
+         
         {
             title: "Peaky Blinders PJ Harvey - Red Right Hand",
             category: "pop",
@@ -129,49 +40,7 @@ $(document).ready(function () {
             category: "pop",
             albumImage: "assets/images/peaky.jpg",
             mp3File: "assets/music/Peaky2.mp3"
-        },
-        {
-            title: "Peaky Blinders Laura Marling - What He Wrote",
-            category: "pop",
-            albumImage: "assets/images/peaky.jpg",
-            mp3File: "assets/music/Peaky3.mp3"
-        },
-        {
-            title: "Peaky Blinders Black Rebel Motorcycle Club - River Styx",
-            category: "pop",
-            albumImage: "assets/images/peaky.jpg",
-            mp3File: "assets/music/Black Rebel Motorcycle Club - River Styx  Peaky Blinders.mp3"
-        },
-        {
-            title: "Peaky Blinders Black Sabbath - The Wizard",
-            category: "pop",
-            albumImage: "assets/images/peaky.jpg",
-            mp3File: "assets/music/Black Sabbath - The Wizard  Peaky Blinders.mp3"
-        },
-        {
-            title: "Peaky Blinders Laura Marling - A Hard Rain's A Gonna Fall",
-            category: "pop",
-            albumImage: "assets/images/peaky.jpg",
-            mp3File: "assets/music/Laura Marling - A Hard Rain's A Gonna Fall  Peaky Blinders.mp3"
-        },
-        {
-            title: "Peaky Blinders Nick Cave And The Bad Seeds - Breathless",
-            category: "pop",
-            albumImage: "assets/images/peaky.jpg",
-            mp3File: "assets/music/Nick Cave And The Bad Seeds - Breathless  Peaky Blinders.mp3"
-        },
-        {
-            title: "Peaky Blinders Royal Blood - Come On Over",
-            category: "pop",
-            albumImage: "assets/images/peaky.jpg",
-            mp3File: "assets/music/Royal Blood - Come On Over  Peaky Blinders.mp3"
-        },
-        {
-            title: "Peaky Blinders Savages - Adore",
-            category: "pop",
-            albumImage: "assets/images/peaky.jpg",
-            mp3File: "assets/music/Savages - Adore  Peaky Blinders.mp3"
-        },
+        },   
     ];
 
     // Function to render the music list based on category
